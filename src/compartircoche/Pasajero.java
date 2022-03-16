b431749150d1196e66dbad867d26487ef315d766
Nheeee <93644452+Nheeee@users.noreply.github.com>
2022-03-16 13:58:05 +0100

    Clase Pasajero

---
+++ src/compartircoche/Pasajero.java
@@ -0,0 +1,15 @@
+
+package personas;
+
+/**
+ *
+ * @author Gabriel Lueiro Rivas
+ */
+public class Pasajero extends Persona {
+
+    public Pasajero(String nombre) {
+        this.nombre = nombre;
+    }
+
+    
+}
