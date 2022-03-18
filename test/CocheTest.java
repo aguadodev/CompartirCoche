56930f81468a2220bb3c79c1142475f17dcd13b4
Óscar Núñez Aguado <101179757+aguadodev@users.noreply.github.com>
2022-03-18 13:25:22 +0100

    CocheTest - Más pruebas

--- test/CocheTest.java
+++ test/CocheTest.java
@@ -16,11 +16,19 @@ public void validarMatricula(){
     assertFalse(Coche.validarMatricula("12A4BBB"));
     assertFalse(Coche.validarMatricula("12.4BBB"));
     assertFalse(Coche.validarMatricula("1234BAB"));
+    assertFalse(Coche.validarMatricula("1234BEB"));
+    assertFalse(Coche.validarMatricula("1234BÑB"));    
     assertFalse(Coche.validarMatricula("1234BB."));
     assertFalse(Coche.validarMatricula("1234B5B"));
     assertFalse(Coche.validarMatricula("1234BúB"));
     assertFalse(Coche.validarMatricula("1234BÚB"));
     assertFalse(Coche.validarMatricula("1234BñB"));
+
+    assertTrue(Coche.validarMatricula("1234BBB"));
+    assertTrue(Coche.validarMatricula("1234BBZ"));
+    assertTrue(Coche.validarMatricula("0000BBB"));
+    assertTrue(Coche.validarMatricula("9999ZZZ"));    
 }        
+
     
 }
